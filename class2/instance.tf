f1e899d4a954ac1e6ce1e8b31681d45f61f413ba
farrukh90 <farrukhsadykov@gmail.com>
2020-08-13 01:00:39 +0000

    added multiple resources

--- class2/instance.tf
+++ class2/instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   associate_public_ip_address = "true"
   key_name      = "${aws_key_pair.class.key_name}"
-  user_data     = "${file("userdata.sh")}"
+  #user_data     = "${file("userdata.sh")}"
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
   availability_zone = "us-east-1a"
 }
