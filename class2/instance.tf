0e22d737f06005b89cba1b674481de9475043536
farrukh90 <farrukhsadykov@gmail.com>
2020-08-13 00:13:39 +0000

    added key

--- class2/instance.tf
+++ class2/instance.tf
@@ -8,5 +8,7 @@ resource "aws_key_pair" "class" {
 resource "aws_instance" "web" {
   ami           = "ami-02354e95b39ca8dec"
   instance_type = "t2.micro"
+  associate_public_ip_address = "true"
+  key_name      = "${aws_key_pair.class.key_name}"
 }
 
