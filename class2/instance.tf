0f5a4bee364481dfc05e393f6894fc0d85dec94e
farrukh90 <farrukhsadykov@gmail.com>
2020-08-13 00:28:16 +0000

    added sec group

--- class2/instance.tf
+++ class2/instance.tf
@@ -1,14 +1,10 @@
-resource "aws_key_pair" "class" {
-  key_name   = "class-key"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
-}
-
-
-
 resource "aws_instance" "web" {
+  #count = 10
   ami           = "ami-02354e95b39ca8dec"
   instance_type = "t2.micro"
   associate_public_ip_address = "true"
   key_name      = "${aws_key_pair.class.key_name}"
+  user_data     = "${file("userdata.sh")}"
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
 }
 
