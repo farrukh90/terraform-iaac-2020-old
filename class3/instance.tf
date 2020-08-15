9b5e820255f18cfbadab84b98b5ba1f29ac08268
farrukh90 <farrukhsadykov@gmail.com>
2020-08-15 17:18:20 +0000

    added null

--- class3/instance.tf
+++ class3/instance.tf
@@ -3,31 +3,4 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   key_name  = "${aws_key_pair.class.key_name}"
   vpc_security_group_ids = ["${aws_security_group.provisioner.id}"]
-  # Push files  to remote server
-  provisioner "file" {
-    connection {
-      host = "${self.public_ip}"
-      type = "ssh"
-      user = "centos"
-      private_key = "${file("~/.ssh/id_rsa")}"
-    }
-    source      = "r1soft.repo"
-    destination = "/tmp/r1soft.repo"
-  }
-  # Execute linux commands on remote machine
-  provisioner "remote-exec" {
-    connection {
-      host = "${self.public_ip}"
-      type = "ssh"
-      user = "centos"
-      private_key = "${file("~/.ssh/id_rsa")}"
-  }
-  inline = [
-      "sudo yum install -y epel-release -y",
-      "sudo yum install httpd -y",
-      "sudo systemctl start httpd",
-      "sudo systemctl enable httpd",
-      "sudo cp  /tmp/r1soft.repo  /etc/yum.repos.d/r1soft.repo"
-    ]
-  }
 }
