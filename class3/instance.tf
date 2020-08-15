37368ac6f12d99bfc361b2be72e351876d988563
farrukh90 <farrukhsadykov@gmail.com>
2020-08-15 16:24:21 +0000

    added sec group

--- class3/instance.tf
+++ class3/instance.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "web" {
   ami           = "ami-02354e95b39ca8dec" 
   instance_type = "t2.micro"
   key_name  = "${aws_key_pair.class.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.provisioner.id}"]
   provisioner "remote-exec" {
     connection {
       host = "${self.public_ip}"
