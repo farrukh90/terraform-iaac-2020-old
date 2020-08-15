915e50bb167337ccecd165e53a83db2793bac813
farrukh90 <farrukhsadykov@gmail.com>
2020-08-15 16:44:00 +0000

    added file

--- class3/instance.tf
+++ class3/instance.tf
@@ -3,6 +3,18 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   key_name  = "${aws_key_pair.class.key_name}"
   vpc_security_group_ids = ["${aws_security_group.provisioner.id}"]
+  # Push files  to remote server
+  provisioner "file" {
+    connection {
+      host = "${self.public_ip}"
+      type = "ssh"
+      user = "ec2-user"
+      private_key = "${file("~/.ssh/id_rsa")}"
+    }
+    source      = "r1soft.repo"
+    destination = "/tmp/r1soft.repo"
+  }
+  # Execute linux commands on remote machine
   provisioner "remote-exec" {
     connection {
       host = "${self.public_ip}"
@@ -15,6 +27,7 @@ resource "aws_instance" "web" {
       "sudo yum install httpd -y",
       "sudo systemctl start httpd",
       "sudo systemctl enable httpd",
+      "sudo cp  /tmp/r1soft.repo  /etc/yum.repos.d/r1soft.repo"
     ]
   }
 }
