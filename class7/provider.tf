c989aa2ead12e4471dfc1923a41eb1c9964037df
farrukh90 <farrukhsadykov@gmail.com>
2020-08-24 23:44:15 +0000

    addd rds

--- class7/provider.tf
+++ class7/provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
     region = "us-east-2"
 }
+
+resource "aws_db_subnet_group" "db" {
+	name = "db"
+	subnet_ids = [
+		"subnet-01fd890eaff2139b7", 
+		"subnet-06e2b411c295d270d", 
+		"subnet-00f8f8482e906ba1f"
+	]
+}
