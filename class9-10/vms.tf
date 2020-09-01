e7b9e939383b7678b8d9f1ec2d1bea37f3177625
farrukh90 <farrukhsadykov@gmail.com>
2020-09-01 01:20:38 +0000

    added more variables

--- class9-10/vms.tf
+++ class9-10/vms.tf
@@ -1,14 +1,14 @@
 resource "azurerm_virtual_machine" "vm1" {
-  name = "vm1"
+  name = "${var.vm1_computername}"
   location = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
   network_interface_ids = ["${azurerm_network_interface.nic1.id}"]
   vm_size = "Standard_DS1_v2"
   storage_image_reference {
-    publisher = "OpenLogic"
-    offer = "CentOS"
-    sku = "7.5"
-    version = "latest"
+   publisher = "${var.publisher}"
+    offer = "${var.offer}"
+    sku = "${var.sku}"
+    version = "${var.version}"
   }
   storage_os_disk {
     name = "myosdisk1"
@@ -31,16 +31,16 @@ resource "azurerm_virtual_machine" "vm1" {
 
 
 resource "azurerm_virtual_machine" "vm2" {
-  name = "vm2"
+  name = "${var.vm2_computername}"
   location = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
   network_interface_ids = ["${azurerm_network_interface.nic2.id}"]
   vm_size = "Standard_DS1_v2"
   storage_image_reference {
-    publisher = "OpenLogic"
-    offer = "CentOS"
-    sku = "7.5"
-    version = "latest"
+    publisher = "${var.publisher}"
+    offer = "${var.offer}"
+    sku = "${var.sku}"
+    version = "${var.version}"
   }
   storage_os_disk {
     name = "myosdisk2"
@@ -66,16 +66,16 @@ resource "azurerm_virtual_machine" "vm2" {
 
 
 resource "azurerm_virtual_machine" "vm3" {
-  name = "vm3"
+  name = "${var.vm3_computername}"
   location = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
   network_interface_ids = ["${azurerm_network_interface.nic3.id}"]
   vm_size = "Standard_DS1_v2"
   storage_image_reference {
-    publisher = "OpenLogic"
-    offer = "CentOS"
-    sku = "7.5"
-    version = "latest"
+    publisher = "${var.publisher}"
+    offer = "${var.offer}"
+    sku = "${var.sku}"
+    version = "${var.version}"
   }
   storage_os_disk {
     name = "myosdisk3"
