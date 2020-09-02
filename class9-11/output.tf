4a77d952ad28a44da7cff8c7f11f92ba39484354
farrukh90 <farrukhsadykov@gmail.com>
2020-09-02 23:45:01 +0000

    added aks

---
+++ class9-11/output.tf
@@ -0,0 +1,40 @@
+output "cluster_id" {
+	value = module.cluster.cluster_id
+}
+output "client_certificate" {
+	value = module.cluster.client_certificate
+}
+output "kube_config" {
+	value = module.cluster.kube_config
+}
+output "cluster_name" {
+	value = module.cluster.cluster_name
+}
+output "client_key" {
+	value = module.cluster.client_key
+}
+output "client_certificate_output" {
+	value = module.cluster.client_certificate_output
+}
+output "cluster_ca_certificate" {
+	value = module.cluster.cluster_ca_certificate
+}
+output "host" {
+	value = module.cluster.host
+}
+output "username" {
+	value = module.cluster.username
+}
+output "password" {
+	value = module.cluster.password
+}
+output "raw_kube_config" {
+	value = module.cluster.raw_kube_config
+}
+output "location" {
+	value = module.cluster.location
+}
+output "kube_config_raw" {
+	value = module.cluster.kube_config_raw
+}
+
